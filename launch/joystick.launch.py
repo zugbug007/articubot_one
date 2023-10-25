39bb8d02ee4c0890f455eaf5bedcf5a00b7beaac
Alan Millington <alan.d.millington@gmail.com>
2023-10-25 15:34:19 +0100

    joystick updates

--- launch/joystick.launch.py
+++ launch/joystick.launch.py
@@ -7,39 +7,24 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
-    use_sim_time = LaunchConfiguration('use_sim_time')
 
     joy_params = os.path.join(get_package_share_directory('articubot_one'),'config','joystick.yaml')
 
     joy_node = Node(
             package='joy',
             executable='joy_node',
-            parameters=[joy_params, {'use_sim_time': use_sim_time}],
+            parameters=[joy_params],
          )
 
     teleop_node = Node(
             package='teleop_twist_joy',
             executable='teleop_node',
             name='teleop_node',
-            parameters=[joy_params, {'use_sim_time': use_sim_time}],
+            parameters=[joy_params],
             remappings=[('/cmd_vel','/diff_cont/cmd_vel_unstamped')]
          )
 
-    twist_stamper = Node(
-            package='twist_stamper',
-            executable='twist_stamper',
-            parameters=[{'use_sim_time': use_sim_time}],
-            remappings=[('/cmd_vel_in','/diff_cont/cmd_vel_unstamped'),
-                        ('/cmd_vel_out','/diff_cont/cmd_vel')]
-         )
-
-
     return LaunchDescription([
-        DeclareLaunchArgument(
-            'use_sim_time',
-            default_value='false',
-            description='Use sim time if true'),
         joy_node,
-        teleop_node,
-        # twist_stamper       
+        teleop_node     
     ])
