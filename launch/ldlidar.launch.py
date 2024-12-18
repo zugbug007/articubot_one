80b66cef6191e38a5fbab2a8ac24102a3e517c19
Alan Millington <zugbug007@users.noreply.github.com>
2024-12-18 17:25:15 +0000

    LD06 Lidar Config

--- launch/ldlidar.launch.py
+++ launch/ldlidar.launch.py
@@ -7,11 +7,11 @@ def generate_launch_description():
     return LaunchDescription([
 
         Node(
-            package='rplidar_ros',
-            executable='rplidar_composition',
+            package='ldlidar_stl_ros2',
+            executable='ldlidar_stl_ros2_node',
             output='screen',
             parameters=[{
-                'serial_port': '/dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.3:1.0-port0',
+                'serial_port': '/dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.3:1.0-port0', 
                 'frame_id': 'laser_frame',
                 'angle_compensate': True,
                 'scan_mode': 'Standard'
