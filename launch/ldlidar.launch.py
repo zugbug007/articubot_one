1d269ee328be42bc20ed5fee3cf91bfa1f386a5d
Alan Millington <alan.d.millington@gmail.com>
2024-12-23 21:52:05 +0000

    Lidar working within the package using LD06 launch file

--- launch/ldlidar.launch.py
+++ launch/ldlidar.launch.py
@@ -24,23 +24,20 @@ Parameter Description:
 def generate_launch_description():
   # LDROBOT LiDAR publisher node
   ldlidar_node = Node(
-      package='ldlidar_ros2',
-      executable='ldlidar_ros2_node',
-      name='ldlidar_publisher_ld06',
+      package='ldlidar_stl_ros2',
+      executable='ldlidar_stl_ros2_node',
+      name='LD06',
       output='screen',
       parameters=[
         {'product_name': 'LDLiDAR_LD06'},
-        {'laser_scan_topic_name': 'scan'},
-        {'point_cloud_2d_topic_name': 'pointcloud2d'},
+        {'topic_name': 'scan'},
         {'frame_id': 'base_laser'},
-        {'port_name': '/dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.3:1.0-port0'},
-        {'serial_baudrate': 230400},
+        {'port_name': '/dev/ttyUSB0'},
+        {'port_baudrate': 230400},
         {'laser_scan_dir': True},
         {'enable_angle_crop_func': False},
-        {'angle_crop_min': 135.0},  # unit is degress
-        {'angle_crop_max': 225.0},  # unit is degress
-        {'range_min': 0.02}, # unit is meter
-        {'range_max': 12.0}   # unit is meter
+        {'angle_crop_min': 135.0},
+        {'angle_crop_max': 225.0}
       ]
   )
 
